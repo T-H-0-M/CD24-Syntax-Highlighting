82e2ae1c114d9220ed4b194e04b1493308d62015
Thomas Bandy <61776913+T-H-0-M@users.noreply.github.com>
2024-08-03 18:49:43 +1000

    Minor Refactor

--- src/extension.ts
+++ src/extension.ts
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "CD24" is now active!');
+	console.log('Congratulations, your extension "cd24-syntax-highlighting" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('CD24.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('cd24-syntax-highlighting.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CD24 Support!');
+		vscode.window.showInformationMessage('Hello World from CD24-Syntax-Highlighting!');
 	});
 
 	context.subscriptions.push(disposable);
